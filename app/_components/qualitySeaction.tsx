cd2564ea7458b7ec4293089e663de54c6da6cc97
Vitor <96vitorleite@gmail.com>
2024-11-20 21:13:09 -0300

    fix: ajust uppercase and marges

--- app/_components/qualitySeaction.tsx
+++ app/_components/qualitySeaction.tsx
@@ -3,6 +3,9 @@ import Image from 'next/image'
 
 const QualitySection = () => {
   return (
+   
+    <>
+
     <div className=' flex flex-row items-center gap-4 py-9 justify-center'>
         <div  className='flex-col rounded-xl bg-white text-black font-semibold border-b-red-600 border-2'>
             <Image className="rounded-s-sm" src={"/lamb.jpg"} alt='' width={350} height={350}/>
@@ -18,9 +21,14 @@ const QualitySection = () => {
             <Image className="rounded-s-sm" src={"/lamb.jpg"} alt='' width={350} height={350}/>
             <p className='py-3 px-2'> this car is awesome!</p>
         </div>
-       
-
     </div>
+
+    <div className='justify-center items-center flex py-9'>
+    <button className='px-6 py-3 bg-red-600 hover:bg-red-700 text-white rounded'>Learn more</button>
+    </div>
+       
+    </>
+   
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Banner from "./_components/banner";
+import CallToAction from "./_components/cta";
 import Navbar from "./_components/navbar";
 import QualitySection from "./_components/qualitySeaction";
 
 export default function Home() {
   return (
-   <div>
+   <>
    <Navbar/>
    <Banner/>
    <QualitySection/>
-   </div>
+   <CallToAction/>
+   </>
   );
 }
