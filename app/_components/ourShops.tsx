1b26e56d9e19747edf86472e31b80893e3478248
Vitor <96vitorleite@gmail.com>
2024-11-21 16:28:21 -0300

    feat: our shops section

---
+++ app/_components/ourShops.tsx
@@ -0,0 +1,61 @@
+import Image from 'next/image'
+import React from 'react'
+
+const OurShops = () => {
+  return (
+
+    <div className='px-56 bg-red-600 text-white font-semibold'>
+
+       <div className='justify-center items-center grid py-9'>
+
+
+      <div className='justify-center items-center flex'>
+       <p className='text-4xl font-semibold py-9'>Visite uma de nossas lojas!</p>   
+       </div>
+       <div className='flex gap-3'>
+            
+            
+            <div>
+            <Image src={'/lamb.jpg'} alt='' width={500} height={300}/>
+              <div className='px-2 py-2 bg-white rounded-b-2xl border-b-black border-2' > 
+                  <p className='py-3 text-black text-3xl font-semibold'>Auto Club</p>
+                  <p className=' py-3 text-black'>Avenida SP, Mato Grosso, Brasil</p>
+                  
+                  <button className='px-6 py-3 bg-red-600 hover:bg-red-700 text-white rounded '>Saiba mais</button>
+            </div>
+            </div>
+
+            <div>
+            <Image src={'/lamb.jpg'} alt='' width={500} height={300}/>
+              <div className='px-2 py-2 bg-white rounded-b-2xl border-b-black border-2' > 
+                  <p className='py-3 text-black text-3xl font-semibold'>Auto Club</p>
+                  <p className=' py-3 text-black'>Avenida SP, Mato Grosso, Brasil</p>
+                  
+                  <button className='px-6 py-3 bg-red-600 hover:bg-red-700 text-white rounded '>Saiba mais</button>
+            </div>
+            </div>
+
+            <div>
+            <Image src={'/lamb.jpg'} alt='' width={500} height={300}/>
+              <div className='px-2 py-2 bg-white rounded-b-2xl border-b-black border-2' > 
+                  <p className='py-3 text-black text-3xl font-semibold'>Auto Club</p>
+                  <p className=' py-3 text-black'>Avenida SP, Mato Grosso, Brasil</p>
+                  
+                  <button className='px-6 py-3 bg-red-600 hover:bg-red-700 text-white rounded '>Saiba mais</button>
+            </div>
+            </div>
+            
+
+            
+        </div>
+
+
+       </div>
+
+      
+    </div>
+  
+         )
+                    }
+
+export default OurShops
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Banner from "./_components/banner";
 import CallToAction from "./_components/cta";
 import Navbar from "./_components/navbar";
 import QualitySection from "./_components/qualitySeaction";
+import CalltoActionSecond from "./_components/cta-second";
+import OurShops from "./_components/ourShops";
 
 export default function Home() {
   return (
@@ -10,6 +12,8 @@ export default function Home() {
    <Banner/>
    <QualitySection/>
    <CallToAction/>
+   <CalltoActionSecond/>
+   <OurShops/>
    </>
   );
 }
