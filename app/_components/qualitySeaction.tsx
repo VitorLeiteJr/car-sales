2638ca0b548b8e61730e48a018aa0e0721066ec0
Vitor <96vitorleite@gmail.com>
2024-11-21 16:18:14 -0300

    chore: ajust marge

--- app/_components/qualitySeaction.tsx
+++ app/_components/qualitySeaction.tsx
@@ -3,10 +3,8 @@ import Image from 'next/image'
 
 const QualitySection = () => {
   return (
-   
-    <div className='px-56'>
-
-    <div className=' flex flex-row items-center gap-4 py-9 justify-center '>
+  <>
+    <div className='flex flex-row items-center gap-4 justify-center px-56 py-9'>
         <div  className='flex-col rounded-xl bg-white text-black font-semibold border-b-red-600 border-2'>
             <Image className="rounded-s-sm" src={"/lamb.jpg"} alt='' width={350} height={350}/>
             <p className='py-3 px-2'> Esse carro foi feito pra vocë!</p>
@@ -23,12 +21,10 @@ const QualitySection = () => {
         </div>
     </div>
 
-    <div className='justify-center items-center flex py-9'>
+    <div className='justify-center items-center flex mb-6'>
     <button className='px-6 py-3 bg-red-600 hover:bg-red-700 text-white rounded'>Saiba mais</button>
     </div>
-       
-    </div>
-   
+   </>
   )
 }
 
