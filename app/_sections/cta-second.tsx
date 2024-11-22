0669466c99ef8e1ab6454be4b71c7c5d204b314d
Vitor <96vitorleite@gmail.com>
2024-11-22 19:19:01 -0300

    refact: all colors

--- app/_sections/cta-second.tsx
+++ app/_sections/cta-second.tsx
@@ -3,19 +3,19 @@ import React from 'react'
 
 const CalltoActionSecond = () => {
   return (
-   <div className='px-56'>
-   <div className='bg-white flex justify-around items-center'>
+   <div className='px-56 bg-red-700'>
+   <div className='flex justify-around items-center'>
 
     <div>
         <Image  src='/renderLamb.png' alt='' width={800} height={700} />
     </div>
 
     <div className='px-14'>
-        <p className='font-semibold text-5xl py-8 text-black'> Elegantes, robustos e completos!</p>
-        <p className='text-xl text-black'>compre nossos carros em alta qualidade!</p>
+        <p className='font-semibold text-5xl py-8 text-white'> Elegantes, robustos e completos!</p>
+        <p className='text-xl text-white'>compre nossos carros em alta qualidade!</p>
 
     <div className='py-7'>
-    <button className='px-6 py-3 bg-red-600 hover:bg-red-700 text-white rounded '>Saiba mais</button>
+    <button className='px-6 py-3 bg-white hover:bg-gray-50 text-black rounded '>Saiba mais</button>
     </div>
     
     </div>
