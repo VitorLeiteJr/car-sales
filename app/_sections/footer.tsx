50170512f338338b4c48410db3231d87333f2b50
Vitor <96vitorleite@gmail.com>
2024-11-23 21:58:09 -0300

    feat: footer section

---
+++ app/_sections/footer.tsx
@@ -0,0 +1,42 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='px-56'>
+      <div className='py-16 flex text-black font-semibold'>
+        <div>
+        <Image src={'/logo.png'} alt='' width={100} height={100}/>
+        </div>
+        <div className='px-32 flex'>
+          <div>
+          <p>Home</p>
+          <p>Sobre</p>
+          <p>Carros</p>
+          <p>Contato</p>
+          </div>
+          <div className='px-64'>
+            <p>Login</p>
+            <p>Carrinho</p>
+            <p>Favoritos</p>
+            <p>Logout</p>
+          </div>
+          <div>
+            <p>Instagram</p>
+            <p>Facebook</p>
+            <p>Youtube</p>
+            <p>WhatsApp</p>
+
+          </div>
+        </div>
+      </div>
+
+        <div className='flex justify-center items-center text-black font-semibold font-mono'>
+          Copyright © 2024 - Auto Club
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,20 @@ import Navbar from "./_sections/navbar";
 import QualitySection from "./_sections/qualitySeaction";
 import CalltoActionSecond from "./_sections/cta-second";
 import OurShops from "./_sections/ourShops";
+import CarList from "./_sections/carList";
+import Footer from "./_sections/footer";
 
 export default function Home() {
   return (
    <>
    <Navbar/>
    <Banner/>
+   <CarList/>
    <QualitySection/>
    <CallToAction/>
-   <CalltoActionSecond/>
    <OurShops/>
+   <CalltoActionSecond/>
+   <Footer/>
    </>
   );
 }
