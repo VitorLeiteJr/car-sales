639d2366b281bbfcd58741de03bd51202e7dd73f
Vitor <96vitorleite@gmail.com>
2024-12-07 19:54:49 -0300

    feat: ensure get the main image and others

---
+++ app/_actions/get-images-car/index.ts
@@ -0,0 +1,47 @@
+import { db } from "@/app/_lib/prisma"
+
+export const GetMainImageCar = async (slug: string, main?: boolean)=>{
+
+
+        
+            const idCar =await db.cars.findFirst({
+                where:{
+                    slug:slug
+                }
+            });
+
+
+    if(main){
+       
+        const mainImage =await db.images.findFirst({
+            where:{
+                carId: idCar?.id
+            }
+        });
+       // console.log(mainImage, idCar?.id);
+
+
+        return mainImage;
+    }
+ 
+}
+
+export const GetImagesCar = async (slug: string)=>{
+
+    const idCar =await db.cars.findFirst({
+        where:{
+            slug:slug
+        }
+    });
+
+    const images = await db.images.findMany({
+        where:{
+            carId: idCar?.id
+        }
+    })
+    return images; 
+
+}
+    
+
+
