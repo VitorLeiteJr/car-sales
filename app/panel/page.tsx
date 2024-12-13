2d752ab1f98fae2a391eb349d966341dbaca5c37
Vitor <96vitorleite@gmail.com>
2024-12-13 20:35:36 -0300

    feat: check token is valid

--- app/panel/page.tsx
+++ app/panel/page.tsx
@@ -1,12 +1,44 @@
-import React from 'react'
+'use client'
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 const PanelPage = () => {
-  return (
-    <div>
-        <p className='text-3xl font-bold text-black'>Panel Page</p>
+
+   const router = useRouter();
+   
+   const [valide, setValide] = useState(false);
+
+   useEffect(()=>{
+
+    const token = localStorage.getItem("token");
+    if(!token) {router.push("/");}else {setValide(true);}
+
+    const validateAuth = async ()=>{
+    const validateToken =await axios.post("/api/panel/validate-token",{token});
+    if(!validateToken.data.status)
+      {
+        router.push("/");
+      setValide(false);
+      }else{
+        setValide(true);
+      }
+    }
+    validateAuth();
+
+   },[]);
+
+    if(valide) return (
+      <div>
+      
+         <p className='text-3xl font-bold text-black'>Panel Page</p>
       
     </div>
-  )
+    );
+
+
+
+  return null;
 }
 
 export default PanelPage
