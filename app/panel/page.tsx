af166816a31dc04bb20adf4b8727a2f9f5ca16fb
Vitor <96vitorleite@gmail.com>
2024-12-16 11:48:48 -0300

    chore: change route name

--- app/panel/page.tsx
+++ app/panel/page.tsx
@@ -15,7 +15,7 @@ const PanelPage = () => {
     if(!token) {router.push("/");}else {setValide(true);}
 
     const validateAuth = async ()=>{
-    const validateToken =await axios.post("/api/panel/validate-token",{token});
+    const validateToken =await axios.post("/api/panel/validate-auth",{token});
     if(!validateToken.data.status)
       {
         router.push("/");
