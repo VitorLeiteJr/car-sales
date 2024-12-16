6ca67a8f14e3fbfd37e8dff20f30d7e40075a883
Vitor <96vitorleite@gmail.com>
2024-12-16 10:21:02 -0300

    chore: change name

---
+++ app/api/panel/validate-auth/route.ts
@@ -0,0 +1,33 @@
+import { db } from "@/app/_lib/prisma";
+import jwt from "jsonwebtoken";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async(req: NextRequest) =>{
+
+    const body = await req.json();
+    const {token,login} = body;
+
+   
+                try{    
+                    
+                    await jwt.verify(token, process.env.JWT_TOKEN as string);
+                    const checkUser = db.user.findFirst({
+                        where: {
+                            login: login,
+                            tokenSession: token
+                        }
+                    })
+
+                    return NextResponse.json({status: true});
+
+
+                }catch{
+
+                        return NextResponse.json({error: "Invalid token", status: false});
+
+                }
+    
+
+
+
+}
