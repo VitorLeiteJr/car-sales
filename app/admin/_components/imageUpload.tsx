fab1c246eb9060015e1f9c98bd3ce5b5f0517585
Vitor <96vitorleite@gmail.com>
2024-12-30 21:44:21 -0300

    feat: news options to edit and ajust layout of upload image button

--- app/admin/_components/imageUpload.tsx
+++ app/admin/_components/imageUpload.tsx
@@ -1,6 +1,8 @@
 "use client"
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { FaUpload } from "react-icons/fa";
+
 
 
 //this script was generate by chatGPT
@@ -63,16 +65,22 @@ const MultiImageUploader = ({id}: MultiImageUploaderProps) => {
   return (
     <div className="p-4 max-w-md mx-auto">
       <label className="block mb-4">
-        <span className="text-gray-700">Upload Images</span>
+        <div className="cursor-pointer hover:text-primary-hover flex gap-2">
+        <FaUpload />
+          <label className="cursor-pointer" htmlFor="inputImages">Clique aqui para carregar uma imagem</label>
         <input
+          id="inputImages"
+         title="Carregar images"
           type="file"
           accept="image/*"
           multiple
           onChange={handleImageUpload}
-          className="mt-2 block w-full text-sm text-gray-500 file:mr-4 file:py-2
-           file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold
-            file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
+          className="hidden"
+         // className="mt-2 block w-full text-sm text-gray-500 file:mr-4 file:py-2
+           //file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold
+            //file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
         />
+        </div>
       </label>
 
       <div className="grid grid-cols-3 gap-4">
--- app/admin/_components/modal/EditCarModal.tsx
+++ app/admin/_components/modal/EditCarModal.tsx
@@ -39,7 +39,7 @@ const EditCarModal = ({handleClose,car}: editCarModalProps) => {
   return (
     <>
     <div className='flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full'>
-<Tabs defaultValue="login" className="w-[400px]">
+<Tabs defaultValue="login" className="max-w-full">
     <TabsContent  value="login">
       <Card className='text-white bg-gray-800'>
 
@@ -52,7 +52,9 @@ const EditCarModal = ({handleClose,car}: editCarModalProps) => {
            Editar o carro: {car.name}
           </CardDescription>
         </CardHeader>
-        <CardContent className="space-y-2">
+        <CardContent >
+          <div className="space-y-2" >
+         
           <div>
             <Input className='hidden' name="id" defaultValue={car.id}></Input>
           </div>
@@ -68,13 +70,37 @@ const EditCarModal = ({handleClose,car}: editCarModalProps) => {
             <Label className='items-center flex mr-3' htmlFor="name">Km: </Label>
             <Input name="km" defaultValue={car.km} required/>
           </div>
+          <div className="space-y-1">
+            <Label className='items-center flex mr-3' htmlFor="mark">Marca: </Label>
+            <Input name="mark" defaultValue={car.mark} required/>
+          </div>
+          <div className="space-y-1">
+            <Label className='items-center flex mr-3' htmlFor="type">Tipo: </Label>
+            <Input name="type" defaultValue={car.type} required/>
+          </div>
+          <div className="space-y-1">
+            <Label className='items-center flex mr-3' htmlFor="transmission">Transmissão: </Label>
+            <Input name="transmission" defaultValue={car.transmission} required/>
+          </div>
+          <div className="space-y-1">
+            <Label className='items-center flex mr-3' htmlFor="fuel">Combustivél: </Label>
+            <Input name="fuel" defaultValue={car.fuel} required/>
+          </div>
+          <div className="space-y-1">
+            <Label className='items-center flex mr-3' htmlFor="slug">Slug: </Label>
+            <Input name="slug" defaultValue={car.slug} required/>
+          </div>
+          <div className="space-y-1">
+            <Label className='items-center flex mr-3 text-primary-hover text-1xl font-semibold' htmlFor="price">Preço: </Label>
+            <Input name="price" defaultValue={car.price} required/>
+          </div>
 
           <div className='space-y-1'>
 
           <MultiImageUploader id={car.id}/>
 
           </div>
-
+          </div>
         </CardContent>
         <CardFooter className='justify-between'>
           <Button type='submit' className='mb-5'>Alterar</Button>
